6c32bd46c4830dc0d296f5c3c43295780c73b8d7
Abhilash Mishra <abhilashmishra89@gmail.com>
2019-02-20 11:17:03 +0900

    added checkHoliday.py
    added checkHoliday.py

---
+++ checkHoliday.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import datetime
+
+
+def holidayJaList():
+    response = requests.get("https://www.googleapis.com/calendar/v3/calendars/japanese__ja%40holiday.calendar.google.com/events?key=APIkeyfromgoogle")
+    data = response._content
+    simple_data = json.loads(data)
+    item= simple_data['items']
+    chugal=[]
+    #print(item)
+    for element in item:
+        chugal.append(element['start']['date'])    
+    return chugal
+
+def isHoliday(aaj):
+    if aaj in holidayJaList():
+        return True
+    else:
+        return False
+
